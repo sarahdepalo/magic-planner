f836e53fbd3f7c00198c462d1c313bca3d49c078
Sarah dePalo <sarahdepalo99@gmail.com>
2021-11-29 14:27:21 -0500

    navbar mobile responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import LoginButton from "../userAuth/LoginButton";
 import logo from "./images/magicPlannerLogo.png";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [active, setIsActive] = useState(false);
   return (
     <>
       <nav>
         <Link to="/">
           <img src={logo} alt="Magic Planner" className="logo" />
         </Link>
-        <ul>
-          <li>Parks</li>
-          <li>Itinerary</li>
-          <li className="login">
-            <LoginButton />
-          </li>
-        </ul>
+        <FontAwesomeIcon icon={faBars} className="menu"  onClick={() => setIsActive(!active)} size="lg"/>
+        <div className={!!active ? "navbar-links active" : "navbar-links"}>
+          <ul>
+            <li>Parks</li>
+            <li>Itinerary</li>
+            <li className="login">
+              <LoginButton />
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
