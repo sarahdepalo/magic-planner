481b3f5ca121855123164bf54bc3933f3bda124d
Sarah dePalo <sarahdepalo99@gmail.com>
2021-11-30 10:52:38 -0500

    added another section to the homepage and did responsiveness

--- frontend/src/components/homepage/HomePage.jsx
+++ frontend/src/components/homepage/HomePage.jsx
@@ -3,7 +3,9 @@ import LoginButton from "../userAuth/LoginButton";
 import LogoutButton from "../userAuth/LogoutButton";
 import UserProfile from "../userProfile/UserProfile";
 import { ReactComponent as Wave } from "./wave.svg";
+import { ReactComponent as ReversedWave } from "./reversedWave.svg";
 import castle from "./images/castle.jpeg";
+import characters from "./images/characters.jpg";
 import "./homepage.scss";
 
 const Homepage = () => {
@@ -40,10 +42,31 @@ const Homepage = () => {
             </div>
           </div>
         </section>
-        <section>
-          <LoginButton />
-          <LogoutButton />
-          <UserProfile />
+        <ReversedWave />
+        <section className="sign-up">
+          <div className="col-container">
+            <div className="col-1">
+              <img
+                src={characters}
+                alt="mickey mouse and alvin the chipmunk disney characters"
+              />
+            </div>
+            <div className="col-2">
+              <span className="circle pink"></span>
+              <div className="text-container">
+                <h2>Make Memories That <br/>Last a Lifetime</h2>
+                <p>
+                  Magic Planner makes it easy to plan out all aspects of your
+                  Disney World vacation. From lodging down to individual
+                  attractions, we've got 'em all. Start planning now.
+                </p>
+                <Link to="/" className="btn btn-primary">
+                  Sign Up
+                </Link>
+              </div>
+              <span className="circle blue"></span>
+            </div>
+          </div>
         </section>
       </main>
     </>
