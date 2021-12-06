804e6ca933c4bf5ed86d6d7f6190a4bfde6f10d7
Sarah dePalo <sarahdepalo99@gmail.com>
2021-12-06 18:43:18 -0500

    started attractions selection:

--- frontend/src/components/attractions/Attractions.jsx
+++ frontend/src/components/attractions/Attractions.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Activities = () => {
-  const [attractions, setAttractions] = useState(null)
+// Select multiple activities, on select change button to FA check. On click again, remove from activities array. When you click next -> redirect to dining for selected park or jump to itinerary -> go to created itinerary. 
+  const [attractions, setAttractions] = useState(null);
+  const [userChoices, setUserChoices] = useState([]);
   const { parkName } = useParams();
  
   useEffect(() => {
@@ -16,14 +18,34 @@ const Activities = () => {
     getAttractions();
   }, []);
 
+  const handleClick = (event, id) => {
+      event.preventDefault();
+      let index
+    if(userChoices.includes(id)) {
+        index = userChoices.indexOf(id);
+        let newArray = [...userChoices];
+        newArray.splice(index, 1);
+        setUserChoices(newArray);
+    } else {
+        setUserChoices([...userChoices, id])
+    }
+    console.log(userChoices)
+  } 
+  // will need to turn each attraction into it's own component so they can manage when the button is clicked and can change the class accordingly
   return (
     <>
     <main>
         <section>
         <h1>{parkName} Attractions</h1>
             {!!attractions ? attractions.map((attraction) => (
-                <div>
+                <div key={`${attraction.activity_name}`}>
                     <h3>{attraction.activity_name}</h3>
+                    <img src={`http://localhost:5000/images/activities/${attraction.activity_image}`}/>
+                    <p>{attraction.activity_type}</p>
+                    <p>{attraction.activity_height}</p>
+                    <p>{attraction.activity_hours}</p>
+                    <p>{attraction.activity_description}</p>
+                    <button className="btn btn-primary" onClick={(event) => handleClick(event, attraction.id)}>Add to Itinerary</button>
                 </div>
             )): (<p>Loading Attractions...</p>)}
         </section>
